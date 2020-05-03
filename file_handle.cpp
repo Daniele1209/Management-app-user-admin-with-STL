3eaab3188e36283a36a505d52063520804446cc9
Mos Daniele <mdie2771@scs.ubbcluj.ro>
2020-05-03 21:03:03 +0300

    more on files

--- file_handle.cpp
+++ file_handle.cpp
@@ -1,48 +1,14 @@
 #pragma once
 #include "file_handle.h"
-#include <sstream>
-#include <string>
-#include <vector>
 #include <fstream>
+#include "custom_exceptions.h"
 
 using namespace std;
 
-vector<string> tokenize(const string& str, char delimiter)
-{
-	vector <string> result;
-	stringstream ss(str);
-	string token;
-	while (getline(ss, token, delimiter))
-		result.push_back(token);
-
-	return result;
+int get_the_size() {
+	return turrets.size();
 }
 
-std::vector<Turret> load_data(std::string filename)
-{
-	ifstream file{ filename };
-	vector<Turret> dataset;
+void set_path() {
 
-	Turret turr{};
-
-	while (file >> turr)
-		dataset.push_back(turr);
-	file.close();
-
-	return dataset;
-}
-
-void split_data(std::vector<Turret> dataset, double factor, std::vector<Turret>& train, std::vector<Turret>& test)
-{
-	int number_for_testing = static_cast<int>(factor * dataset.size());
-	size_t number_for_training = dataset.size() - number_for_testing;
-
-	int i = 0;
-	for (i = 0; i < number_for_training; i++)
-		train.push_back(dataset[i]);
-	while (i < dataset.size())
-	{
-		test.push_back(dataset[i]);
-		i++;
-	}
 }
--- file_handle.h
+++ file_handle.h
@@ -3,3 +3,42 @@
 #include <vector>
 #include "turret.h"
 #include <iostream>
+
+using namespace std;
+
+class watchman_repo
+{
+protected:
+	vector<Turret> turrets;
+	string f_path;
+
+public:
+	watchman_repo() { f_path = ""; };
+	watchman_repo(string path);
+
+	int get_the_size();
+	void set_path(string path);
+	void add_to_mylist(Turret turr);
+	virtual void save_file(vector<Turret>&) = 0;
+
+	Turret turret_at_pos(int pos);
+	Turret find_turret(string location);
+};
+
+class watchman_HTML : public watchman_repo
+{
+public:
+	watchman_HTML() { f_path = ""; };
+	watchman_HTML(string path) { f_path = path; };
+
+	void save_file(const vector<Turret>& t) override;
+};
+
+class watchman_CSV : public watchman_repo
+{
+public:
+	watchman_CSV() { f_path = ""; };
+	watchman_CSV(string path) { f_path = path; };
+
+	void save_file(const vector<Turret>& t) override;
+};
